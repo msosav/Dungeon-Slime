3be923c4b815ad1e8ae3fc0391f4fd51374c5311
Miguel <miguelsosa2003@gmail.com>
2025-09-05 14:36:58 -0500

    Chapter 04: Creating a Class Library

---
+++ DungeonSlime/Game1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using MonoGameLibrary;
+
+namespace DungeonSlime;
+
+public class Game1 : Core
+{
+    public Game1() : base(title: "Dungeon Slime", width: 1280, height: 720, fullScreen: false)
+    {
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        // TODO: use this.Content to load your game content here
+
+        base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Moccasin);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
